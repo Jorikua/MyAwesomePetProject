629b32defa0fc6ec5474072a1d5dc42d71158121
Seva <kaganovychv@gmail.com>
2019-04-18 22:08:42 +0300

    Added strategies to failure callbacks in views.

--- app/src/main/java/com/example/myawesomepetproject/details/presentation/DetailsView.kt
+++ app/src/main/java/com/example/myawesomepetproject/details/presentation/DetailsView.kt
@@ -1,9 +1,12 @@
 package com.example.myawesomepetproject.details.presentation
 
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.example.myawesomepetproject.details.presentation.model.FullPostViewModel
 
 interface DetailsView : MvpView{
   fun onLoadDetailsSuccess(fullPostViewModel: FullPostViewModel)
+  @StateStrategyType(OneExecutionStateStrategy::class)
   fun onLoadDetailsFailure()
 }
--- app/src/main/java/com/example/myawesomepetproject/posts/presentation/PostsView.kt
+++ app/src/main/java/com/example/myawesomepetproject/posts/presentation/PostsView.kt
@@ -1,9 +1,12 @@
 package com.example.myawesomepetproject.posts.presentation
 
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.example.myawesomepetproject.posts.presentation.model.PostViewModel
 
 interface PostsView: MvpView {
   fun onLoadPostsSuccess(list: List<PostViewModel>)
+  @StateStrategyType(OneExecutionStateStrategy::class)
   fun onLoadPostsFailure()
 }
